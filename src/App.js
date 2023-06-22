b2e2166776525c5c4975da60568bf4df25752a41
Gabriel Loh <gabrielloh@Gabriels-MacBook-Pro.local>
2023-06-22 11:33:12 +0800

    Made changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
-import UserTable from "./components/Table/UserTable";
-import "./components/Table/index.css";
-
 import Sidebar from "./components/Sidebar/Sidebar";
 import LoginPage from "./components/Login/LoginPage";
+
+import UserTable from "./components/Table/UserTable";
+import PendingUsers from "./components/Table/PendingUsers";
+import "./components/Table/index.css";
 
 function App() {
 	return (
 		<Container fluid className="min-vh-100">
 			<Row>
 				<Sidebar />
-				<UserTable />
+				{/* <UserTable /> */}
+				<PendingUsers />
+
 			</Row>
 		</Container>
-
+		
 		// <div>
 		// 	<LoginPage />
 		// </div>
---
+++ src/components/Table/PendingUsers.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { data } from './data.js';
+import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
+
+function PendingUsers() {
+  const [contacts, setContacts] = useState(data);
+  const [search, setSearch] = useState('');
+
+  return (
+    <Col xs={10}>
+        <Container>
+            <h1 className='text-center mt-4'>Pending Users</h1>
+            <Form>
+              <InputGroup className='my-3'>
+
+                {/* onChange for search */}
+                <Form.Control
+                  onChange={(e) => setSearch(e.target.value)}
+                  placeholder='Search users'
+                />
+              </InputGroup>
+            </Form>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>NRIC</th>
+                  <th>Contact</th>
+                  <th></th>
+
+                </tr>
+              </thead>
+              <tbody>
+                {data
+                  .filter((item) => {
+                    const Name = `${item.Name}`;
+                    const NRIC = item.NRIC;
+                    const Address = item.Address;
+                    return (
+                      search.toLowerCase() === '' ||
+                      Name.toLowerCase().includes(search.toLowerCase()) ||
+                      NRIC.toLowerCase().includes(search.toLowerCase()) ||
+                      Address.toLowerCase().includes(search.toLowerCase())
+                    );
+                  })
+                  .map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.Name}</td>
+                      <td>{item.NRIC}</td>
+                      <td>{item.Address}</td>
+                      <td align="center"><Button>View User</Button></td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+        </Container>
+      </Col>    
+  );
+}
+
+export default PendingUsers;
