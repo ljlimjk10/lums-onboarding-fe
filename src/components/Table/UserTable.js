1d2e5ba480069d542908484699f3ebe51e49bdf4
Gabriel Loh <gabrielloh@Gabriels-MacBook-Pro.local>
2023-06-22 11:01:38 +0800

    Existing Users page with table

--- src/components/Table/UserTable.js
+++ src/components/Table/UserTable.js
@@ -6,6 +6,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { data } from './data.js';
 import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
 
 function UserTable() {
   const [contacts, setContacts] = useState(data);
@@ -32,27 +33,32 @@ function UserTable() {
                   <th>NRIC</th>
                   <th>Contact</th>
                   <th>Address</th>
+                  <th></th>
+                  
                 </tr>
               </thead>
               <tbody>
                 {data
                   .filter((item) => {
-                    const fullName = `${item.first_name} ${item.last_name}`;
-                    const email = item.email;
-                    const phone = item.phone;
+                    const Name = `${item.Name}`;
+                    const NRIC = item.NRIC;
+                    const Contact = item.Contact;
+                    const Address = item.Address;
                     return (
                       search.toLowerCase() === '' ||
-                      fullName.toLowerCase().includes(search.toLowerCase()) ||
-                      email.toLowerCase().includes(search.toLowerCase()) ||
-                      phone.toLowerCase().includes(search.toLowerCase())
+                      Name.toLowerCase().includes(search.toLowerCase()) ||
+                      NRIC.toLowerCase().includes(search.toLowerCase()) ||
+                      Contact.toLowerCase().includes(search.toLowerCase()) ||
+                      Address.toLowerCase().includes(search.toLowerCase())
                     );
                   })
                   .map((item, index) => (
                     <tr key={index}>
-                      <td>{item.first_name}</td>
-                      <td>{item.last_name}</td>
-                      <td>{item.email}</td>
-                      <td>{item.phone}</td>
+                      <td>{item.Name}</td>
+                      <td>{item.NRIC}</td>
+                      <td>{item.Contact}</td>
+                      <td>{item.Address}</td>
+                      <td align="center"><Button>View User</Button></td>
                     </tr>
                   ))}
               </tbody>
