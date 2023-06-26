627e2df1b4b5b63383ced5fae228044c7c9d6c73
Gabriel Loh <gabrielloh@Gabriels-MacBook-Pro.local>
2023-06-26 13:34:08 +0800

    Pending users is now mobile responsive

--- src/components/Users/PendingUsersTable.js
+++ src/components/Users/PendingUsersTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -9,120 +9,120 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
-
 function PendingUsersTable() {
-	const [search, setSearch] = useState("");
-	const [selectedStatus, setSelectedStatus] = useState("");
+  const [search, setSearch] = useState("");
+  const [selectedStatus, setSelectedStatus] = useState("");
+  const [isMobile, setIsMobile] = useState(false);
 
-	const clearFilters = () => {
-		setSearch("");
-		setSelectedStatus("");
-	};
+  const clearFilters = () => {
+    setSearch("");
+    setSelectedStatus("");
+  };
 
-	return (
-		<Col>
-			<Container>
-				<h1 className="text-center mt-4">Pending Users</h1>
-				<Form>
-					<InputGroup className="my-3">
-						<ButtonGroup aria-label="Basic example">
-							<Button
-								variant={
-									selectedStatus === "New"
-										? "info"
-										: "primary"
-								}
-								style={{ borderRadius: 0, zIndex: 0}}
-								onClick={() => setSelectedStatus("New")}
-							>
-								New
-							</Button>
-							<Button
-								variant={
-									selectedStatus === "Editing"
-										? "info"
-										: "primary"
-								}
-								style={{ zIndex: 0}}
-								onClick={() => setSelectedStatus("Editing")}
-							>
-								Editing
-							</Button>
-							<Button
-								variant={
-									selectedStatus === "Rejected"
-										? "info"
-										: "primary"
-								}
-								style={{ borderRadius: 0,  zIndex: 0 }}
-								onClick={() => setSelectedStatus("Rejected")}
-							>
-								Rejected
-							</Button>
-						</ButtonGroup>
-						<Form.Control
-							onChange={(e) => setSearch(e.target.value)}
-							placeholder="Search users"
-						/>
-						<Button
-							variant="primary"
-							onClick={clearFilters}
-							style={{ borderRadius: 0 }}
-						>
-							Clear Filters
-						</Button>
-					</InputGroup>
-				</Form>
-				<Table striped bordered hover>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>NRIC</th>
-							<th>Address</th>
-							<th>Status</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{data
-							.filter((item) => {
-								const Name = `${item.Name}`;
-								const NRIC = item.NRIC;
-								const Address = item.Address;
-								const Status = item.Status;
-								return (
-									(search.toLowerCase() === "" ||
-										Name.toLowerCase().includes(
-											search.toLowerCase()
-										) ||
-										NRIC.toLowerCase().includes(
-											search.toLowerCase()
-										) ||
-										Address.toLowerCase().includes(
-											search.toLowerCase()
-										)) &&
-									(selectedStatus === "" ||
-										Status === selectedStatus)
-								);
-							})
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Col>
+      <Container>
+        <h1 className="text-center mt-4">Pending Users</h1>
+        <Form>
+          <InputGroup className="my-3">
+            <ButtonGroup aria-label="Basic example">
+              <Button
+                variant={selectedStatus === "New" ? "info" : "primary"}
+                style={{ borderRadius: 0, zIndex: 0 }}
+                onClick={() => setSelectedStatus("New")}
+              >
+                New
+              </Button>
+              <Button
+                variant={selectedStatus === "Editing" ? "info" : "primary"}
+                style={{ zIndex: 0 }}
+                onClick={() => setSelectedStatus("Editing")}
+              >
+                Editing
+              </Button>
+              <Button
+                variant={selectedStatus === "Rejected" ? "info" : "primary"}
+                style={{ borderRadius: 0, zIndex: 0 }}
+                onClick={() => setSelectedStatus("Rejected")}
+              >
+                Rejected
+              </Button>
+            </ButtonGroup>
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search users"
+            />
+            <Button
+              variant="primary"
+              onClick={clearFilters}
+              style={{ borderRadius: 0, zIndex: 0 }}
+            >
+              Clear Filters
+            </Button>
+          </InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>NRIC</th>
+              {!isMobile && (
+                <>
+                  <th>Address</th>
+                  <th>Status</th>
+                </>
+              )}
+			  <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              .filter((item) => {
+                const Name = `${item.Name}`;
+                const NRIC = item.NRIC;
+                const Address = item.Address;
+                const Status = item.Status;
+                return (
+                  (search.toLowerCase() === "" ||
+                    Name.toLowerCase().includes(search.toLowerCase()) ||
+                    NRIC.toLowerCase().includes(search.toLowerCase()) ||
+                    Address.toLowerCase().includes(search.toLowerCase())) &&
+                  (selectedStatus === "" || Status === selectedStatus)
+                );
+              })
               // Only display users with status other than "Accepted" when no filters are selected
               .filter((item) => item.Status !== "Accepted")
-							.map((item, index) => (
-								<tr key={index}>
-									<td>{item.Name}</td>
-									<td>{item.NRIC}</td>
-									<td>{item.Address}</td>
-									<td>{item.Status}</td>
-									<td align="center">
-										<Button>View User</Button>
-									</td>
-								</tr>
-							))}
-					</tbody>
-				</Table>
-			</Container>
-		</Col>
-	);
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.Name}</td>
+                  <td>{item.NRIC}</td>
+                  {!isMobile && (
+                    <>
+                      <td>{item.Address}</td>
+                      <td>{item.Status}</td>
+                    </>
+                  )}
+				  <td align="center">
+                        <Button>View User</Button>
+                	</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
+    </Col>
+  );
 }
 
 export default PendingUsersTable;
