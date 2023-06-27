c67a9159a43aa21988f3cda5afd94f164f751a94
Gabriel Loh <gabrielloh@Gabriels-MacBook-Pro.local>
2023-06-27 10:54:52 +0800

    Uploaded responsive Pending users and Existing Users

--- src/components/Users/UserView.js
+++ src/components/Users/UserView.js
@@ -7,40 +7,32 @@ import Form from 'react-bootstrap/Form';
 import TextBox from "../Layout/Views/TextBox";
 import Image from 'react-bootstrap/Image';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 
 function UserView() {
     return (
-        <React.Fragment>
-            <Col>
-                <div className="justify-content-center" style={{ marginLeft: "5%" }}>
-                    <TextBox Label="Name" pholder="Jozef Rayhill" style={{marginTop:"100px"}} disabled={true}/>
-                    <TextBox Label="NRIC" pholder="K8101852S" disabled={true}/>
-                    <TextBox Label="Address" pholder="25 Dapin Center" disabled={true}/>
-                    <TextBox Label="Contact" pholder="63837130" disabled={true}/>
-                    <TextBox Label="Telegram" pholder="ulxymmp" disabled={true}/>
-                    <TextBox Label="Affiliation" pholder="Lumens" disabled={true}/>
+        <Container fluid>
+            <Row>
+                <Col style={{border:"1px solid red"}} lg={12} md={12} xs={12}>
+                    <h3><TextBox Label="User Information"/></h3>
 
-                    <div className="justify-content-center" style={{marginTop:"100px"}}>
-                        <Row>
-                            <Col>
-                                <Image src="https://picsum.photos/200" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Image src="https://picsum.photos/200" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Image src="https://picsum.photos/200" />
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
-
-            </Col>
-        </React.Fragment>            
+                    <hr />
+                </Col>
+                <Col style={{border:"1px solid red"}} lg={6} md={6} xs={12}>
+                    Name
+                    NRIC
+                    Address
+                </Col>
+                <Col style={{border:"1px solid red"}} lg={6} md={6} xs={12}>
+                    Contact
+                    Telegram 
+                    Entity
+                </Col>
+                <Col style={{border:"1px solid red"}} lg={12} md={12} xs={12}>
+                    User Documents
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
