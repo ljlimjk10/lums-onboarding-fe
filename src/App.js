f565b7dcc6b2a1d39364fa3215c3a8248eeb42ca
ljlimjk <ljlimjk10@gmail.com>
2023-06-29 16:54:34 +0800

    added routing features for sidebar. changed path to follow naming conventions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
-import Sidebar from "./components/Sidebar/Sidebar";
-import LoginPage from "./components/Login/LoginPage";
 import ExistingUsersTable from "./components/Users/ExistingUsersTable";
 import PendingUsersTable from "./components/Users/PendingUsersTable";
-import UserView from "./components/Users/UserView";
-
-import Button from "./components/Layout/Views/Button";
-import ContentCard from "./components/Layout/Views/ContentCard";
-import LabelText from "./components/Layout/Views/LabelText";
-import { Nav } from "react-bootstrap";
+// import UserView from "./components/Users/UserView";
+import Post from "./components/Posts/Post/Post";
 
 function App() {
 	const postData = {
@@ -25,12 +16,14 @@ function App() {
 		date: "test date (create new date obj with backend data)",
 	};
 	return (
-		<Router>
-			<Navbar />
-			<ExistingUsersTable />
-			{/* <PendingUsersTable />  */}
-			{/* <UserView vis="visible"/>  */}
-		</Router>
+		<Routes>
+			<Route path="/" element={<Navbar />}>
+				{/* using pending users table as homepage while developing homepage */}
+				<Route index element={<PendingUsersTable />} />
+				<Route path="users" element={<ExistingUsersTable />} />
+				<Route path="posts" element={<Post postData={postData} />} />
+			</Route>
+		</Routes>
 	);
 }
 
