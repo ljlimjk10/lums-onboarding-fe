89c256c3fd2b87ab4f4e7b3fdc51a024ed4db4d2
Gabriel Loh <gabrielloh@Gabriels-MacBook-Pro.local>
2023-07-10 13:48:40 +0800

    edited dashboard and fontSizes

--- src/components/Layout/Views/Dashboard_element.js
+++ src/components/Layout/Views/Dashboard_element.js
@@ -9,7 +9,7 @@ function Dashboard_element(props){
             <Col lg={props.lg} md={props.md} xs={props.xs} style={{ height: "300px", marginBottom: props.mBottom, marginTop: props.mTop }}>
                 <Row style={{marginLeft:"1px"}}>{props.title}</Row>
                 <div className="d-flex justify-content-center align-items-center h-100">
-                    <Button variant="danger" style={{width:"100%", height:"100%"}}>{props.content}</Button>
+                    <Button variant="danger" style={{width:"100%", height:"100%", fontSize: props.fSize }}>{props.content}</Button>
                 </div>
             </Col>
         </>
